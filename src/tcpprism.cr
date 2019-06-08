486ddea17b2e855ca367743a2d55f84b330de3bf
Carl Hörberg <carl.hoerberg@gmail.com>
2019-06-08 21:35:56 +0200

    improved cli handling

--- src/tcpprism.cr
+++ src/tcpprism.cr
@@ -6,20 +6,27 @@ module TCPPrism
     listen = ""
     forward = ""
     mirror = ""
-    OptionParser.parse! do |parser|
+    parser = OptionParser.new do |parser|
       parser.banner = "Usage: tcpprism [arguments]"
-      parser.on("-l", "--listen=ADDR", "Address and port to listen on") { |v| listen = v }
-      parser.on("-f", "--forward=ADDR", "Address and port to forward traffic to") { |v| forward = v }
-      parser.on("-m", "--mirror=ADDR", "Address and port to mirror traffic to") { |v| mirror = v }
+      parser.on("-l", "--listen=ADDR", "Address and port to listen on (required)") { |v| listen = v }
+      parser.on("-f", "--forward=ADDR", "Address and port to forward traffic to (required)") { |v| forward = v }
+      parser.on("-m", "--mirror=ADDR", "Address and port to mirror traffic to (required)") { |v| mirror = v }
       parser.on("-h", "--help", "Show this help") { puts parser; exit }
       parser.invalid_option do |flag|
         STDERR.puts "ERROR: #{flag} is not a valid option."
-        STDERR.puts parser
-        exit(1)
+        abort parser
+      end
+      parser.missing_option do |flag|
+        STDERR.puts "ERROR: #{flag} requires a value, eg. localhost:1234"
+        abort parser
       end
     end
-    s = Server.new(listen, forward, mirror)
-    s.run
+    parser.parse!
+    if listen.empty? || forward.empty? || mirror.empty?
+      STDERR.puts "Required argument missing"
+      abort parser
+    end
+    Server.new(listen, forward, mirror).run
   end
 end
 
